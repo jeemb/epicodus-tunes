2cc69de3871ee18dd4993b62faac8d4adf3aceb0
Felix Oporto Lopez and Jennifer Beem <felix@>
2017-04-03 14:49:01 -0700

    implement provider, service, and injector

---
+++ src/app/mock-albums/mock-albums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-albums',
+  templateUrl: './mock-albums.component.html',
+  styleUrls: ['./mock-albums.component.css']
+})
+export class MockAlbumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
